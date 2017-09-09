9d3f3d20cd02cfe5da02957a2934293e3a31120a
Oleg Sikorskiy <olegsik@gmail.com>
2017-09-09 18:06:06 +0300

    Added thymeleaf support

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,9 +3,12 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalTime;
 
 @Controller
 @EnableAutoConfiguration
@@ -19,6 +22,12 @@ public class GreetingController {
     @ResponseBody
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return greeter.say(name);
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("time", LocalTime.now().toString());
+        return "index";
     }
 
     @Autowired
